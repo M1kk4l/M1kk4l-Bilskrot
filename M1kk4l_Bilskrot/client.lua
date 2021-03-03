defdfc82f8880d626a1bb9d2662d9d7cd1adc48d
Sh4dow8080 <78806525+Sh4dow8080@users.noreply.github.com>
2021-03-03 23:47:34 +0100

    En masse lort
    Ændret mythic_notify til pNotify grundet der er flere serverer som allerede har pNotify
    Fixed så NPC'ens spawn location er baseret på bilens position (Ikke 100% sikker på den altid spawner foran)
    Gjordt config filen simpler
    Flyttet webhook fra config filen ind i server scriptet for at undgå modders kan dumpe scriptet og spam webhooken

--- M1kk4l_Bilskrot/client.lua
+++ M1kk4l_Bilskrot/client.lua
@@ -25,169 +25,114 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1)
         if Skrot then
-        for k,v in pairs(M1kk4l.Sted) do
-        if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()),v.x,v.y,v.z, true) < 35 then
-            DrawMarker(36, v.x,v.y,v.z, 0, 0, 0, 0, 0, 0, 1.5, 1.5, 1.5001, 60, 163, 49, 250, 0, 0, 0, 1)
-            if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), v.x,v.y,v.z, true ) < 1 then
-                DrawText3D(v.x,v.y,v.z+1.0, "[~b~E~w~] Skrotte køretøjet")
-                if IsControlJustPressed (1, M1kk4l.Key) then
-                    HT.TriggerServerCallback('M1kk4l:Client:CheckJob', function(Job)
-                        if Job == true then
-                            Skrot = false
-                            local ped = GetPlayerPed(-1)
-                            local vehicle = GetVehiclePedIsIn(ped, false)
-                            if (IsPedSittingInAnyVehicle(ped)) then 
-                                exports['M1kk4l_progressBars']:startUI(10000, "Skrotter køretøjet...")
-                                RequestModel(GetHashKey('s_m_y_armymech_01'))
-                                while not HasModelLoaded('s_m_y_armymech_01') do
-                                    Citizen.Wait(1)
-                                end
-                                for k,b in pairs(M1kk4l.NPC) do
-                                SetVehicleDoorOpen(vehicle, 4, false)
-                                Ped2 = CreatePed(4, 0x62CC28E2, b.x, b.y, b.z-1, b.h, false, true)
-                                SetEntityHeading(Ped2, b.h)
-                                FreezeEntityPosition(Ped2, true)
-                                SetEntityInvincible(Ped2, true)
-                                SetBlockingOfNonTemporaryEvents(Ped2, true)
-                                RequestAnimDict("anim@amb@facility@missle_controlroom@")
-                                    while not HasAnimDictLoaded("anim@amb@facility@missle_controlroom@") do
-                                        Citizen.Wait(1)
+            for k,sted in pairs(M1kk4l.Steder) do
+                if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()),sted.x,sted.y,sted.z, true) < 35 then
+                    DrawMarker(36, sted.x,sted.y,sted.z, 0, 0, 0, 0, 0, 0, 1.5, 1.5, 1.5001, 60, 163, 49, 250, 0, 0, 0, 1)
+                    if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), sted.x,sted.y,sted.z, true ) < 1 then
+                        DrawText3D(sted.x,sted.y,sted.z+1.0, "[~b~E~w~] Skrotte køretøjet")
+                        if IsControlJustPressed (1, M1kk4l.Key) then
+                            HT.TriggerServerCallback('M1kk4l:Client:CheckJob', function(Job)
+                                if Job == true then
+                                    Skrot = false
+                                    local ped = GetPlayerPed(-1)
+                                    local vehicle = GetVehiclePedIsIn(ped)
+                                    if vehicle ~= 0 then
+                                        exports['M1kk4l_progressBars']:startUI(10000, "Skrotter køretøjet...")
+
+                                        RequestModel(GetHashKey('s_m_y_armymech_01'))
+                                        while not HasModelLoaded('s_m_y_armymech_01') do
+                                            Citizen.Wait(1)
+                                        end
+
+                                        RequestAnimDict("anim@amb@facility@missle_controlroom@")
+                                        while not HasAnimDictLoaded("anim@amb@facility@missle_controlroom@") do
+                                            Wait(1)
+                                        end
+
+                                        SetVehicleDoorOpen(vehicle, 4, false)
+                                        ped = CreatePed(4, 0x62CC28E2, GetEntityCoords(ped), 0, false, true)
+                                        
+                                        local dimension = GetModelDimensions(GetEntityModel(vehicle))
+                                        AttachEntityToEntity(ped, vehicle, GetPedBoneIndex(ped, 6286), 0.0, dimension.y * -1 + 0.1 , dimension.z + 1.0, 0.0, 0.0, 180.0, 0.0, false, false, true, false, true)                                
+
+                                        FreezeEntityPosition(ped, true)
+                                        SetEntityInvincible(ped, true)
+                                        SetBlockingOfNonTemporaryEvents(ped, true)
+
+                                        TaskPlayAnim(ped,"anim@amb@facility@missle_controlroom@", "idle", 8.0, 1.0, -1, 0, 0, 0, 0, 0)
+                                        FreezeEntityPosition(vehicle, true)
+
+                                        Wait(10000)
+
+                                        FreezeEntityPosition(vehicle, false)
+                                        SetVehicleDoorShut(vehicle, 4, true)
+                                        TaskLeaveVehicle(ped, vehicle)
+
+                                        Wait(1000)
+                                        
+                                        while DoesEntityExist(vehicle) do
+                                            Wait(500)
+                                            DeleteGivenVehicle(vehicle)
+                                            DeleteEntity(ped)
+                                            TriggerServerEvent("M1kk4l:BilskrotPenge")
+                                            Skrot = true
+                                        end
+                                    else
+                                        TriggerEvent("pNotify:SendNotification",{text = "Du skal sidde i et køretøj.",type = "error",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})    
+                                        Skrot = true
                                     end
-                                TaskPlayAnim(Ped2,"anim@amb@facility@missle_controlroom@", "idle", 8.0, 1.0, -1, 0, 0, 0, 0, 0)
-                                FreezeEntityPosition(vehicle, true)
-                                Citizen.Wait(10000)
-                                FreezeEntityPosition(vehicle, false)
-                                SetVehicleDoorShut(vehicle, 4, true)
-                                TaskLeaveVehicle(ped, vehicle)
-                                Citizen.Wait(1000)
-                                while DoesEntityExist(vehicle) do
-                                    Citizen.Wait(500)
-                                    DeleteGivenVehicle(vehicle)
-                                    DeleteEntity(Ped2)
-                                    TriggerServerEvent("M1kk4l:BilskrotPenge")
-                                    Skrot = true
-                                end
-                                end
                                 else
-                                    exports['mythic_notify']:DoHudText('error', 'Du skal sidde i et køretøj.')
-                                    Skrot = true
-                            end
-                        else
-                            TriggerEvent("pNotify:SendNotification",{text = "Du er ikke Mekaniker",type = "error",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})    
-                          end
-                    end)
+                                    TriggerEvent("pNotify:SendNotification",{text = "Du er ikke Mekaniker",type = "error",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})    
+                                end
+                            end)
+                        end
+                    end
                 end
             end
         end
     end
-    end
-end
 end)
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1)
-        if Skrot then
-        for k,v in pairs(M1kk4l.Sted2) do
-        if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()),v.x,v.y,v.z, true) < 35 then
-            DrawMarker(36, v.x,v.y,v.z, 0, 0, 0, 0, 0, 0, 1.5, 1.5, 1.5001, 60, 163, 49, 250, 0, 0, 0, 1)
-            if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), v.x,v.y,v.z, true ) < 1 then
-                DrawText3D(v.x,v.y,v.z+1.0, "[~b~E~w~] Skrotte køretøjet")
-                if IsControlJustPressed (0, M1kk4l.Key) then
-                    HT.TriggerServerCallback('M1kk4l:Client:CheckJob', function(Job)
-                        if Job == true then
-                            Skrot = false
-                            local ped = GetPlayerPed(-1)
-                            local vehicle = GetVehiclePedIsIn(ped, false)
-                            if (IsPedSittingInAnyVehicle(ped)) then 
-                                exports['M1kk4l_progressBars']:startUI(10000, "Skrotter køretøjet...")
-                                RequestModel(GetHashKey('s_m_y_armymech_01'))
-                                while not HasModelLoaded('s_m_y_armymech_01') do
-                                    Citizen.Wait(1)
-                                end
-                                for k,b in pairs(M1kk4l.NPC2) do
-                                SetVehicleDoorOpen(vehicle, 4, false)
-                                Ped2 = CreatePed(4, 0x62CC28E2, b.x, b.y, b.z-1, b.h, false, true)
-                                SetEntityHeading(Ped2, b.h)
-                                FreezeEntityPosition(Ped2, true)
-                                SetEntityInvincible(Ped2, true)
-                                SetBlockingOfNonTemporaryEvents(Ped2, true)
-                                RequestAnimDict("anim@amb@facility@missle_controlroom@")
-                                    while not HasAnimDictLoaded("anim@amb@facility@missle_controlroom@") do
-                                        Citizen.Wait(1)
-                                    end
-                                TaskPlayAnim(Ped2,"anim@amb@facility@missle_controlroom@", "idle", 8.0, 1.0, -1, 0, 0, 0, 0, 0)
-                                FreezeEntityPosition(vehicle, true)
-                                Citizen.Wait(10000)
-                                FreezeEntityPosition(vehicle, false)
-                                SetVehicleDoorShut(vehicle, 4, true)
-                                TaskLeaveVehicle(ped, vehicle)
-                                Citizen.Wait(1000)
-                                while DoesEntityExist(vehicle) do
-                                    Citizen.Wait(500)
-                                    DeleteGivenVehicle(vehicle)
-                                    DeleteEntity(Ped2)
-                                    TriggerServerEvent("M1kk4l2:BilskrotPenge")
-                                    Skrot = true
-                                end
-                                end
-                                else
-                                    exports['mythic_notify']:DoHudText('error', 'Du skal sidde i et køretøj.')
-                                    Skrot = true
-                            end
-                        else
-                            TriggerEvent("pNotify:SendNotification",{text = "Du er ikke Mekaniker",type = "error",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})    
-                          end
-                    end)
-                end
-            end
-        end
-    end
-    end
-end
-end)
-
 
 function DrawText3D(x, y, z, text)
     local onScreen, _x, _y = World3dToScreen2d(x, y, z)
-    local px, py, pz = table.unpack(GetGameplayCamCoords())
-  
-    SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(1)
-    AddTextComponentString(text)
-    DrawText(_x, _y)
+    if onScreen then
+        SetTextScale(0.35, 0.35)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x, _y)
         local factor = (string.len(text)) / 370
-    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+        DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+    end
 end
 
 function DeleteGivenVehicle(veh, timeoutMax)
     local timeout = 0 
-    
+
     SetEntityAsMissionEntity(veh, true, true)
     DeleteVehicle(veh)
 
     if (DoesEntityExist(veh)) then
-        exports['mythic_notify']:DoHudText('error', 'Kunne ikke skrotte køretøjet. Prøv igen.')
+        TriggerEvent("pNotify:SendNotification",{text = "Kunne ikke skrotte køretøjet. Prøv igen.",type = "error",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})    
 
-        while (DoesEntityExist(veh) and timeout < timeoutMax) do 
+        while (DoesEntityExist(veh) and timeout < timeoutMax) do
             DeleteVehicle(veh)
 
-            if (not DoesEntityExist(veh) ) then 
-                exports['mythic_notify']:DoHudText('success', 'Køretøjet er skrottet.')
-            end 
+            if (not DoesEntityExist(veh) ) then
+                TriggerEvent("pNotify:SendNotification",{text = "Køretøjet er skrottet.",type = "success",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})    
+            end
 
-            timeout = timeout + 1 
+            timeout = timeout + 1
             Citizen.Wait(500)
 
             if (DoesEntityExist(veh) and (timeout == timeoutMax - 1)) then
-                exports['mythic_notify']:DoHudText('error', 'Kunne ikke skrotte køretøjet. Prøv igen.')
-            end 
-        end 
-    else 
-        exports['mythic_notify']:DoHudText('success', 'Køretøjet er skrottet.')
-    end 
-end 
+                TriggerEvent("pNotify:SendNotification",{text = "Kunne ikke skrotte køretøjet. Prøv igen.",type = "error",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})    
+            end
+        end
+    else
+        TriggerEvent("pNotify:SendNotification",{text = "Køretøjet er skrottet.",type = "success",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})    
+    end
+end
--- M1kk4l_Bilskrot/server.lua
+++ M1kk4l_Bilskrot/server.lua
@@ -1,19 +1,19 @@
 --[[
-███╗   ███╗  ███╗  ██╗  ██╗██╗  ██╗  ██╗██╗██╗
-████╗ ████║░████║  ██║ ██╔╝██║ ██╔╝ ██╔╝██║██║
-██╔████╔██║██╔██║  █████═╝ █████═╝ ██╔╝ ██║██║
-██║╚██╔╝██║╚═╝██║  ██╔═██╗ ██╔═██╗ ███████║██║
-██║ ╚═╝ ██║███████╗██║ ╚██╗██║ ╚██╗╚════██║███████╗
-╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝     ╚═╝╚══════╝
-M1kk4l © 2021 | Alle Rettigheder Forbeholdes.
---]] 
+    ███╗   ███╗  ███╗  ██╗  ██╗██╗  ██╗  ██╗██╗██╗
+    ████╗ ████║░████║  ██║ ██╔╝██║ ██╔╝ ██╔╝██║██║
+    ██╔████╔██║██╔██║  █████═╝ █████═╝ ██╔╝ ██║██║
+    ██║╚██╔╝██║╚═╝██║  ██╔═██╗ ██╔═██╗ ███████║██║
+    ██║ ╚═╝ ██║███████╗██║ ╚██╗██║ ╚██╗╚════██║███████╗
+    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝     ╚═╝╚══════╝
+    M1kk4l © 2021 | Alle Rettigheder Forbeholdes.
+--]]
+
+M1kk4l.Webhook = "DIN WEBHOOK" -- Her kan du skrive din webhook ind
 
 HT = nil
-
-TriggerEvent('HT_base:getBaseObjects', function(obj) 
-    HT = obj 
+TriggerEvent('HT_base:getBaseObjects', function(obj)
+    HT = obj
 end)
-
 
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
@@ -33,22 +33,12 @@ end)
 
 RegisterServerEvent('M1kk4l:BilskrotPenge')
 AddEventHandler('M1kk4l:BilskrotPenge', function()
-  local source = source
-  local user_id = vRP.getUserId({source})
-  local penge2 = {M1kk4l.Penge[1], M1kk4l.Penge[2], M1kk4l.Penge[3], M1kk4l.Penge[4], M1kk4l.Penge[5], M1kk4l.Penge[6], M1kk4l.Penge[7], M1kk4l.Penge[8], M1kk4l.Penge[9], M1kk4l.Penge[10], M1kk4l.Penge[11], M1kk4l.Penge[12], M1kk4l.Penge[14], M1kk4l.Penge[15], M1kk4l.Penge[16], M1kk4l.Penge[17], M1kk4l.Penge[18], M1kk4l.Penge[19], M1kk4l.Penge[20], M1kk4l.Penge[21], M1kk4l.Penge[22], M1kk4l.Penge[23], M1kk4l.Penge[24], M1kk4l.Penge[25], M1kk4l.Penge[26], M1kk4l.Penge[27], M1kk4l.Penge[28], M1kk4l.Penge[29], M1kk4l.Penge[30],}
-  local penge = (penge2[math.random(#penge2)])
-    vRP.giveMoney({user_id,penge})
-    PerformHttpRequest(M1kk4l.Webhook, function(err, text, headers) end, 'POST', json.encode({username = "M1kk4l Skrot", content = "**ID: "..user_id.."**  Skrottede lige et køretøj! Og modtog **" ..penge.. "** DKK"}), { ['Content-Type'] = 'application/json' })
-    TriggerClientEvent("pNotify:SendNotification", source,{text = "Du modtog <b style='color: #4E9350'>"..penge.." DKK</b>. For at skrotte køretøjet.", type = "success", queue = "global", timeout = 5000, layout = "centerRight",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
-end)
-
-RegisterServerEvent('M1kk4l2:BilskrotPenge')
-AddEventHandler('M1kk4l2:BilskrotPenge', function()
-  local source = source
-  local user_id = vRP.getUserId({source})
-  local penge2 = {M1kk4l.Penge[1], M1kk4l.Penge[2], M1kk4l.Penge[3], M1kk4l.Penge[4], M1kk4l.Penge[5], M1kk4l.Penge[6], M1kk4l.Penge[7], M1kk4l.Penge[8], M1kk4l.Penge[9], M1kk4l.Penge[10], M1kk4l.Penge[11], M1kk4l.Penge[12], M1kk4l.Penge[14], M1kk4l.Penge[15], M1kk4l.Penge[16], M1kk4l.Penge[17], M1kk4l.Penge[18], M1kk4l.Penge[19], M1kk4l.Penge[20], M1kk4l.Penge[21], M1kk4l.Penge[22], M1kk4l.Penge[23], M1kk4l.Penge[24], M1kk4l.Penge[25], M1kk4l.Penge[26], M1kk4l.Penge[27], M1kk4l.Penge[28], M1kk4l.Penge[29], M1kk4l.Penge[30],}
-  local penge = (penge2[math.random(#penge2)])
-    vRP.giveMoney({user_id,penge})
-    PerformHttpRequest(M1kk4l.Webhook, function(err, text, headers) end, 'POST', json.encode({username = "M1kk4l Skrot", content = "**ID: "..user_id.."**  Skrottede lige et køretøj! Og modtog **" ..penge.. "** DKK"}), { ['Content-Type'] = 'application/json' })
-    TriggerClientEvent("pNotify:SendNotification", source,{text = "Du modtog <b style='color: #4E9350'>"..penge.." DKK</b>. For at skrotte køretøjet.", type = "success", queue = "global", timeout = 5000, layout = "centerRight",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
+    local user_id = vRP.getUserId({source})
+    if vRP.hasGroup({user_id, M1kk4l.Group}) then
+        math.randomseed(os.time());math.random();math.random();math.random()
+        local penge = math.random(M1kk4l.PengeMin, M1kk4l.PengeMax)
+        vRP.giveMoney({user_id,penge})
+        PerformHttpRequest(M1kk4l.Webhook, function(err, text, headers) end, 'POST', json.encode({username = "M1kk4l Skrot", content = "**ID: "..user_id.."**  Skrottede lige et køretøj! Og modtog **" ..penge.. "** DKK"}), { ['Content-Type'] = 'application/json' })
+        TriggerClientEvent("pNotify:SendNotification", source,{text = "Du modtog <b style='color: #4E9350'>"..penge.." DKK</b>. For at skrotte køretøjet.", type = "success", queue = "global", timeout = 5000, layout = "centerRight",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
+    end
 end)
